f80c0123e85509785c7bb23257dfc7559592c301
Shivam Nath <shivamnath027@gmail.com>
2024-03-25 16:26:50 +0530

    BLOG

--- src/main/java/com/blog/readIt/ReadItApplication.java
+++ src/main/java/com/blog/readIt/ReadItApplication.java
@@ -1,14 +1,38 @@
 package com.blog.readIt;
 
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+//
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+//public class ReadItApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ReadItApplication.class, args);
+//	}
+//	
+//}
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
+import org.springframework.data.mongodb.core.MongoTemplate;
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class ReadItApplication {
-
+public class ReadItApplication implements CommandLineRunner{
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ReadItApplication.class, args);
 	}
 	
+	@Override
+	public void run(String... args) throws Exception {
+	
+		System.out.println("-------SHIVAM-----------"+ mongoTemplate.getCollectionNames());
+
+	}
 }
