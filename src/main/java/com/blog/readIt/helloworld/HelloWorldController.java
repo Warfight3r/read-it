ef1b810d70b9f46b66218957643ef4a9fd72df71
Rishabh Ranjan <rishabh9050@gmail.com>
2024-03-25 14:44:17 +0530

    Index.html added and merged with SpringBoot

--- src/main/java/com/blog/readIt/helloworld/HelloWorldController.java
+++ src/main/java/com/blog/readIt/helloworld/HelloWorldController.java
@@ -1,12 +1,15 @@
 package com.blog.readIt.helloworld;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class HelloWorldController {
-	@GetMapping(path= "/helloworld")
+	
+	@GetMapping("/")
 	public String helloworld() {
-		return "hello world";
+		return "index";
 	}
 }
